585be06b3b2e0a9a79e2434a1ea6fd756fdd5301
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-07-26 22:16:33 +0100

    fixes

--- python.py
+++ python.py
@@ -1,2 +1,16 @@
-for i in range(10):
-    print('Hello Miskiat')
+# for i in range(10):
+#     print('Hello Miskiat')
+
+firstName = "Badmus"
+print (firstName*5)
+
+print (5 + 4)
+
+print (5//4)
+print (5/4)
+print (5*4)
+print(5**4)
+print(5%4)
+
+
+ 
