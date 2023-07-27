5961d10a951873158cdcda6a2708bbb11b251757
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-07-27 23:41:57 +0100

    if elif else

--- python.py
+++ python.py
@@ -1,16 +1,22 @@
 # for i in range(10):
 #     print('Hello Miskiat')
 
-firstName = "Badmus"
-print (firstName*5)
+# firstName = "Badmus"
+# print (firstName*5)
 
-print (5 + 4)
+# print (5 + 4)
 
-print (5//4)
-print (5/4)
-print (5*4)
-print(5**4)
-print(5%4)
-
-
- 
+# print (5//4)
+# print (5/4) 
+# print (5*4)
+# print(5**4)
+# print(5%4)
+# user = 'Miskiat'
+# print (user[4])
+age  = 18
+if age > 12:
+   print('naso naso')
+name ="Miskyyyy"
+if name is "Miskyyy":
+    print("thats meeee")
+elif    
